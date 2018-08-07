95913274c2417338a46f7c518dcd72705a4b6d60
jarangos <jarangos@LES005476.corp.capgemini.com>
2018-08-07 09:18:34 +0200

    Cosas

---
+++ src/main/java/com/jarango/modelo/Estudiante.java
@@ -0,0 +1,13 @@
+package com.jarango.modelo;
+
+public class Estudiante {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
